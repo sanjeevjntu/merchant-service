cfb37b1b4020537794acb1ad9b37b7446778ec5e
rajanalasanjeev <rajanala.sanjeev@gmail.com>
2020-04-22 01:34:43 -0400

    using @Id or @MongoId

--- src/main/java/com/sanjeev/retail/merchant/Merchant.java
+++ src/main/java/com/sanjeev/retail/merchant/Merchant.java
@@ -2,15 +2,16 @@ package com.sanjeev.retail.merchant;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
-import org.springframework.data.mongodb.core.mapping.MongoId;
 
 @Document(collection = "merchants")
 @Setter
 @Getter
 public class Merchant {
 
-    @MongoId
+    //@MongoId
+    @Id
     private String id;
     private String name;
     private String phone;
