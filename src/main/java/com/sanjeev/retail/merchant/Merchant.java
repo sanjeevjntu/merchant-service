7a11641daf7fbf98d62cd6e7984305d526836cec
rajanalasanjeev <rajanala.sanjeev@gmail.com>
2020-04-22 01:56:27 -0400

    Added annotations for validation

--- src/main/java/com/sanjeev/retail/merchant/Merchant.java
+++ src/main/java/com/sanjeev/retail/merchant/Merchant.java
@@ -5,6 +5,10 @@ import lombok.Setter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
 @Document(collection = "merchants")
 @Setter
 @Getter
@@ -13,8 +17,13 @@ public class Merchant {
     //@MongoId
     @Id
     private String id;
+    @NotBlank
     private String name;
+    @NotBlank
+    @Size(min = 10, max = 20)
     private String phone;
+    @Email
     private String email;
+    @NotBlank
     private String address;
 }
